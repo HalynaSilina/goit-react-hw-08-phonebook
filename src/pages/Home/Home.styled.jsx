8cf2135768c3ee80a1c4278ad1fa7d1eb3c8f1c1
HalynaSilina <halynasilina@gmail.com>
2023-06-26 03:51:55 +0300

    +

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,24 +1,23 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.main`
+export const Container = styled.div`
+  min-height: 100vh;
   background-color: #c9f7ff;
-  background-image: url(./images/start-mobile.jpg);
-  background-position: center;
+  background-image: url(../../images/start-mobile.jpg);
   background-size: cover;
   background-repeat: no-repeat;
   background-attachment: fixed;
   color: #321c01;
   @media screen and (min-width: 425px) {
-    background-image: url(./images/start-tablet.jpg);
+    background-image: url(../../images/start-tablet.jpg);
   }
   @media screen and (min-width: 1024px) {
-    background-image: url(./images/start.jpg);
-    background-position: right bottom;
+    background-image: url(../../images/start.jpg);
   }
   @media (min-device-pixel-ratio: 2),
     (-webkit-min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
-    background-image: url(./images/start2x.jpg);
+    background-image: url(../../images/start2x.jpg);
   }
 `;
